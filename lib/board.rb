728383bc74f556d70339c62cf768e7daec258853
Zach T <ztrokey@gmail.com>
2021-04-01 22:08:17 -0600

    Add more to valid placement

--- lib/board.rb
+++ lib/board.rb
@@ -33,7 +33,12 @@ class Board
   end
 
   def valid_placement?(boat, coordinates)
-    length_check(boat, coordinates) && consecutive_check(boat, coordinates) && !backwards?(boat, coordinates)
+    # length_check(boat, coordinates) && cnc(boat, coordinates) #fails b1c1d1
+    length_check(boat, coordinates) && clc(boat,coordinates) #a1a2
+    # cnc(boat, coordinates) && clc(boat,coordinates) #a1a2
+    # length_check(boat, coordinates) && cnc(boat, coordinates) && clc(boat,coordinates)
+    # require "pry"; binding.pry
+    # length_check(boat, coordinates) && consecutive_check(boat, coordinates) && !backwards?(boat, coordinates)
     # boat.length == coordinates.length
   end
 
@@ -41,29 +46,29 @@ class Board
     boat.length == coordinates.length
   end
 
-  def consecutive_check(boat,coordinates)
-    result = []
-    coordinates.each do |coordinate|
-      result.push(coordinate.split(''))
-    end
-    letters = []
-    numbers = []
-    result.each do |split_coordinate|
-      letters.push(split_coordinate[0]) && numbers.push(split_coordinate[1])
-    end
-    result = false
-    numbers.each_cons(2) do |number|
-     if number[1].to_i - number[0].to_i == -1
-       if letters.uniq.count == 1 || numbers.uniq.count == 1
-         result = true
-         # require "pry"; binding.pry
-       end
-     end
-    end
-    result
-  end
+  # def cnc(boat,coordinates)
+  #   result = []
+  #   coordinates.each do |coordinate|
+  #     result.push(coordinate.split(''))
+  #   end
+  #   letters = []
+  #   numbers = []
+  #   result.each do |split_coordinate|
+  #     letters.push(split_coordinate[0].ord) && numbers.push(split_coordinate[1].to_i)
+  #   end
+  #   results = false
+  #   numbers.each_cons(2) do |number|
+  #     # require "pry"; binding.pry
+  #     if number[1].to_i - number[0].to_i == 1
+  #       if letters.uniq.count == 1 || numbers.uniq.count == 1
+  #         results = true
+  #       end
+  #     end
+  #   end
+  #   results
+  # end
 
-  def backwards?(boat, coordinates)
+  def clc(boat,coordinates)
     result = []
     coordinates.each do |coordinate|
       result.push(coordinate.split(''))
@@ -71,20 +76,87 @@ class Board
     letters = []
     numbers = []
     result.each do |split_coordinate|
-      letters.push(split_coordinate[0]) && numbers.push(split_coordinate[1])
+      letters.push(split_coordinate[0].ord) && numbers.push(split_coordinate[1].to_i)
     end
-    result = false
-    numbers.each_cons(2) do |number|
-      number[1].to_i - number[0].to_i == 1
-      result = true
+    results = false
+    letters.each_cons(2) do |letter|
+      if letter[1] - letter[0] == 1
+        numbers.each_cons(2) do |number|
+          if number[1].to_i - number[0].to_i == 0
+            if letters.uniq.count == 1 || numbers.uniq.count == 1
+          results = true
+            end
+          end
+        end
+      end
     end
-    result
+    results
   end
 end
 
 
 
+# if letters[1].to_i - letters[0].to_i == 0 && numbers[1] - numbers[0] == 1 || letters[1].to_i - letters[0].to_i == 1 && numbers[1] - numbers[0] == 0
 
+
+
+
+
+
+
+
+
+
+
+
+
+  # Dont erase this!!!!
+  # def consecutive_check(boat,coordinates)
+  #   result = []
+  #   coordinates.each do |coordinate|
+  #     result.push(coordinate.split(''))
+  #   end
+  #   letters = []
+  #   numbers = []
+  #   result.each do |split_coordinate|
+  #     letters.push(split_coordinate[0].ord) && numbers.push(split_coordinate[1].to_i)
+  #   end
+  #   result = false
+  #   numbers.each_cons(2) do |number|
+  #    if number[1].to_i - number[0].to_i == -1
+       # if letters.uniq.count == 1 || numbers.uniq.count == 1
+  #        result = true
+  #        # require "pry"; binding.pry
+  #      end
+  #     end
+  #   end
+  #   result
+  # end
+
+  # def backwards?(boat, coordinates)
+  #   result = []
+  #   coordinates.each do |coordinate|
+  #     result.push(coordinate.split(''))
+  #   end
+  #   letters = []
+  #   numbers = []
+  #   result.each do |split_coordinate|
+  #     letters.push(split_coordinate[0]) && numbers.push(split_coordinate[1])
+  #   end
+  #   result = false
+  #   numbers.each_cons(2) do |number|
+  #     number[1].to_i - number[0].to_i == 1
+  #     result = true
+  #   end
+  #   result
+  # end
+
+
+# if letters[1].to_i - letters[0].to_i == 0 && numbers[1] - numbers[0] == 1 || letters[1].to_i - letters[0].to_i == 1 && numbers[1] - numbers[0] == 0
+
+# letter[0] - letter[1] == 0 && numbers[0] - numbers[1] == 1
+# or
+# letters[0] - letters[1] == 1 && numbers[0] - numbers[1] == 0
 
 
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -50,8 +50,35 @@ RSpec.describe Board do
       expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
       expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
       expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-      expect(board.backwards?(cruiser, ["A3", "A2", "A1"])).to eq(true)
     end
 
+    it 'checks for diagonal coordinates' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+    end
+
+    xit 'checks for valid placement' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.cnc(submarine, ["A1", "A2"])).to eq(true)
+      expect(board.cnc(submarine, ["B1", "B2"])).to eq(true)
+      expect(board.clc(submarine, ["A1", "B1"])).to eq(true)
+      expect(board.clc(submarine, ["B1", "C1"])).to eq(true)
+    end
+
+    it 'checks for valid placement' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      # require "pry"; binding.pry
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+    end
   end
 end
