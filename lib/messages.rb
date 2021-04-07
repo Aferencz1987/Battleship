c42f0a918aba6d83596d4010dd93a4a705f8fa6f
Zach T <ztrokey@gmail.com>
2021-04-07 08:39:24 -0600

    fix spelling issue in messages

--- lib/messages.rb
+++ lib/messages.rb
@@ -8,7 +8,7 @@ class Messages
   end
 
   def place_ship_message
-    "You will be playing against the computer! The computer will pick theres right now.\n Pick your cells by typing your selection below. \nA cruiser needs three cells. A submarie needs 2. The selections must be consecutive and not diagonal. \nHappy battling!"
+    "You will be playing against the computer! The computer will place thier ships right now.\n Pick your cells by typing your selection below. \nA cruiser needs three cells. A submarie needs 2. The selections must be consecutive and not diagonal. \nHappy battling!"
   end
 
   def printz
